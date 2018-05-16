5cf42ff524b45501d39f16b808b3b5dcde464fb8
matheuspb95 <matheuspb95@gmail.com>
2018-05-16 19:13:08 -0400

    Fisrt

---
+++ Assets/HeroMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroMovement : MonoBehaviour {
+	Rigidbody2D body;
+	public float velocity;
+	// Use this for initialization
+	void Start () {
+		body = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+		Move(input, velocity);
+	}
+
+	void LateUpdate(){
+		float x = Mathf.Round(transform.position.x * 10) / 100;
+		//print(x);
+		float y = Mathf.Round(transform.position.y * 10) / 100;
+		//transform.position = new Vector2(x, y);
+	}
+
+	public void Move(Vector2 direction, float velocity){
+		body.velocity = direction * velocity;
+	}
+}
---
+++ Assets/QuickerEnemy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuickerEnemy : MonoBehaviour {
+	Rigidbody2D body;
+	public float velocity;
+	// Use this for initialization
+	void Start () {
+		body = GetComponent<Rigidbody2D>();
+		body.velocity = Vector2.one * velocity;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		if(Mathf.Abs(body.velocity.x) != Mathf.Abs(body.velocity.y)){
+			print("Error");
+			body.velocity = new Vector2(Mathf.Sign(body.velocity.x) * velocity, 
+										Mathf.Sign(body.velocity.y) * velocity);
+		}		
+	}
+}
